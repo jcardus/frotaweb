8516511b1ee449f28d25a76eb84364030e1578ec
jcardus <joaquim_cardeira@hotmail.com>
2024-11-03 01:21:36 +0000

    unovis charts

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,1 +1,19 @@
-// place files you want to import through the `$lib` alias in this folder.
+export const countBy = (array, key) => {
+    const countMap = array.reduce((result, currentValue) => {
+        const groupKey = currentValue[key];
+
+        if (!result[groupKey]) {
+            result[groupKey] = 0;
+        }
+
+        result[groupKey]++;
+
+        return result;
+    }, {});
+
+    // Convert the result object to an array of objects
+    return Object.keys(countMap).map(category => ({
+        category,
+        count: countMap[category]
+    }));
+};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,57 @@
-<h1>Dashboard</h1>
+<script lang='ts'>
+    import {
+        VisSingleContainer,
+        VisDonut,
+        VisBulletLegend,
+        VisXYContainer,
+        VisTimeline
+    } from '@unovis/svelte'
+    import { countBy } from '$lib'
+    let { data } = $props();
+
+    const grouped = countBy(data.devices, 'status')
+
+    const legendItems = Object.entries(grouped).map(([_, grouped]) => ({
+        name: grouped.category.charAt(0).toUpperCase() + grouped.category.slice(1),
+        color: {
+            offline: '#ff6b7d',
+            online: '#a6cc74',
+            unknown: '#4d8cfd'
+        }[grouped.category]
+    }))
+
+
+    let times = data.devices.map(d => ({
+        timestamp: new Date(d.lastUpdate),
+        length: 1,
+        type: d.name
+    }))
+    const x = (d) => d.timestamp
+</script>
+
+<div class="grid grid-cols-3 gap-3 p-2 text-center">
+
+        <div>
+        <h3>Estados dos dispositivos</h3>
+        <VisBulletLegend items={legendItems} />
+        <VisSingleContainer>
+            <VisDonut
+                    value={d => d.count}
+                    data="{grouped}"
+                    color="{d => ({
+                        offline: '#ff6b7d',
+                        online: '#a6cc74',
+                        unknown: '#4d8cfd'
+                    }[d.category])}"
+                    showEmptySegments={true}
+                    padAngle={0.01}
+                    arcWidth={50}
+            />
+        </VisSingleContainer>
+    </div>
+    <div>
+        <VisXYContainer data="{times}">
+            <VisTimeline {x}/>
+        </VisXYContainer>
+    </div>
+</div>
