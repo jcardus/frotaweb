52ccb3128f86a5f2da86b30d9a84a109464db3a0
Joaquim Cardeira <joaquim_cardeira@hotmail.com>
2026-04-24 18:31:08 +0100

    allow origin

--- functions/api/positions.js
+++ functions/api/positions.js
@@ -14,5 +14,11 @@ export const onRequest = ({request}) => {
 
     const url = new URL(request.url);
     url.pathname = "/traccar" + url.pathname;
-    return Response.redirect(url.toString(), 302);
+    return new Response(null, {
+        status: 302,
+        headers: {
+            'Location': url.toString(),
+            'Access-Control-Allow-Origin': '*',
+        }
+    });
 }
